37efbf9c6655177428cabacd995ee34a4c1f5664
joao vitor <githubconta2021@gmail.com>
2023-04-03 20:03:00 -0400

    finalizando as config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const exphbs = require('express-handlebars'); 
 const session = require('express-session'); 
-const FileStora = require('session-file-store')(session)
+const FileStore = require('session-file-store')(session)
 const flash = require('express-flash'); 
 
 // execuçao do express
@@ -10,7 +10,57 @@ const app = express();
 // conecçao com banco de dados mysql 
 const conn = require('./db/conn');
 
-// alteraçao feita
+
+// template engine
+app.engine('handlebars',  exphbs.engine()); 
+app.set('view engine', 'handlebars');
+
+
+//  receber respota do body 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json());
+
+
+// session middleware
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret', 
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore( {
+            logFn: function() {}, 
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }), 
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now + 360000),
+            httpOnly: true
+        } 
+    }),
+) 
+
+// flash messages 
+app.use(flash())
+
+
+// public path
+app.use(express.static('public'));
+
+
+// set session to res
+app.use((req , res , next ) => {
+   if (res.session.userId) {
+    res.locals.session = req.session
+   }
+
+   next()
+
+})
 
 
 // config de servidor
